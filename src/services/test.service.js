468378f0108a77c0cd5dd6e827ca304123a14b0f
Cristian Villafane <cristianv129@hotmail.com>
2021-03-19 02:08:03 -0300

    Agregado metodos a test service

--- src/services/test.service.js
+++ src/services/test.service.js
@@ -1,12 +1,22 @@
 import http from "../utils/http-common";
-//import authHeader from "./auth-header";
+import authHeader from "./auth-header";
 
 function create(data, username) {
     return http.post(`/test/create/${username}`, data)
 }
 
+function getByUser() {
+    return http.get(`/test/get-user`, { headers: authHeader()})
+}
+
+function remove(id) {
+    return http.delete(`/test/delete/${id}`, { headers: authHeader()})
+}
+
 const methods = {
-    create
+    create,
+    getByUser,
+    remove
 }
 
 export default methods;
