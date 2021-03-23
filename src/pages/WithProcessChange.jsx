1658dd3bf6798057399c013d96ec212a9e3b3188
Cristian Villafane <cristianv129@hotmail.com>
2021-03-22 22:15:35 -0300

    Desarrollado HOC de Procesar cambios de email

---
+++ src/pages/WithProcessChange.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import RequestChangeService from '../services/requestchange.service';
+import PageContainer from '../components/PageContainer';
+import { CircularProgress, Typography } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+
+function withProcessChange(action, WrappedComponent) {
+
+    return () => {
+        const { id } = useParams();
+        const [loading, setLoading] = useState(true);
+        const [errorMessage, setErrorMessage] = useState('')
+        const [changeAccepted, setChangeAccepted] = useState(false);
+
+        useEffect(() => {
+            if (action === "confirm") {
+                RequestChangeService.confirm(id)
+                    .then(res => setChangeAccepted(true))
+                    .catch(err => setErrorMessage(err.response.data.message))
+                    .finally(() => setLoading(false))
+            } else {
+                RequestChangeService.cancel(id)
+                    .then(res => setChangeAccepted(true))
+                    .catch(err => setErrorMessage(err.response.data.message))
+                    .finally(() => setLoading(false))
+            }
+        })
+
+        if(loading) return (
+            <PageContainer align="center">
+                <div>
+                    <CircularProgress/>
+                    <Typography variant="h6">Procesando cambios</Typography> 
+                </div>
+            </PageContainer>
+        )
+        return (
+            <PageContainer>
+                {
+                    changeAccepted ? 
+                        <WrappedComponent /> 
+                        : 
+                        <Typography variant="h6" color="secondary" style={{margin: '2em'}}>
+                            {'Error: ' + errorMessage}
+                        </Typography> 
+                }
+            </PageContainer>
+        )
+    }
+}
+
+export default withProcessChange;
